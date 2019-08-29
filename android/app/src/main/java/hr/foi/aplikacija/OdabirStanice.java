6bb383ae156ab290e07f9d6199536759ff3d7c44
Marta Fiorencis <marfioren@foi.hr>
2019-08-29 13:11:47 +0200

    Odabir odgovarajućeg modula ovisno o kliku na radio button na formi.

--- android/app/src/main/java/hr/foi/aplikacija/OdabirStanice.java
+++ android/app/src/main/java/hr/foi/aplikacija/OdabirStanice.java
@@ -13,15 +13,15 @@ public class OdabirStanice extends AppCompatActivity{
         RadioButton rT = findViewById(R.id.OdabirTeks);
         RadioButton rG = findViewById(R.id.OdabirGraf);
         if(rT.isChecked()){
-            this.formatIspisa="Tekst";
+            this.formatIspisa=1;
 
         }
         else{
-            this.formatIspisa="Graf";
+            this.formatIspisa=0;
         }
 
     }
-    String formatIspisa;
+    int formatIspisa;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +34,34 @@ public class OdabirStanice extends AppCompatActivity{
         startService(serviceIntent);
 
     }
+   private void pokreniManager(String imeStanice){
 
+       ModuleManager mm= ModuleManager.getInstance();
+       ViewGroup parent = (ViewGroup) findViewById(R.id.parent);
+       mm.setActivity(this);
+       DohvaćanjePodataka podaci= new DohvaćanjePodataka();
+       podaci.ImeStanice(imeStanice);
+       podaci.dohvatiPodatke();
+       mm.pokreniModul(parent, podaci.pod, podaci.podv, this.formatIspisa);
+
+   }
     public void Stanica1_podaci(View view) {
          provjerir();
         setContentView(R.layout.fragmenti);
-        ModuleManager mm= ModuleManager.getInstance();
-
-        ViewGroup parent = (ViewGroup) findViewById(R.id.parent);
-mm.setActivity(this);
-        DohvaćanjePodataka podaci= new DohvaćanjePodataka();
-        podaci.ImeStanice("c76a33ba-8f0e-43b8-a8b8-508fa8ed49a0");
-        podaci.dohvatiPodatke();
-
-mm.pokreniModul(parent, podaci.pod, podaci.podv);
 
     }
 
     public void Stanica2_podaci(View view) {
         provjerir();
+        setContentView(R.layout.fragmenti);
+        pokreniManager("ca4bb982-ceb0-4c07-b9f2-848351928903");
 
     }
 
     public void Stanica3_podaci(View view) {
         provjerir();
+        setContentView(R.layout.fragmenti);
+        pokreniManager("c76a33ba-8f0e-43b8-a8b8-508fa8ed49a0");
 
 
     }
--- android/grafoviprikaz/src/main/java/hr/foi/grafoviprikaz/grafPrikaz.java
+++ android/grafoviprikaz/src/main/java/hr/foi/grafoviprikaz/grafPrikaz.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
